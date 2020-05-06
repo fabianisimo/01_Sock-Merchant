d4dc3bde798956aefc39d3ee9b0ca27ed2582702
Fabianisimo <fabianbravotoro@gmail.com>
2020-05-06 19:11:21 -0400

    version final

--- Sock_merchant.py
+++ Sock_merchant.py
@@ -1,42 +1,33 @@
 import random
 
-
 def entrada_variables():
-    n = random.randint(1, 10)
+    n = random.randint(1, 20)
     ar = []
     for a in range(n):
-        i = random.randint(1, 10)
+        i = random.randint(1, 20)
         ar.append(i)
     return [n, ar]
-
-def printea_datos(entrada):
-    n = entrada[0]
-    ar = entrada[1]
-    print (n)
-    print (ar)
-    #print (len(ar))
 
 entrada = entrada_variables()
 n = entrada[0]
 ar = entrada[1]
 
-entrada = printea_datos(entrada)
+def sockMerchant(n,ar):
+    cant_pares = 0
+    posiciones_tomadas = []
+    for i in range(n-1):
+        if i in posiciones_tomadas:
+            pass
+        else:
+            for o in range(i+1, n):
+                if ar[i] == ar[o]:
+                    cant_pares = cant_pares + 1
+                    posiciones_tomadas.append(i)
+                    posiciones_tomadas.append(o)
+    return cant_pares
 
 
-cant_pares = 0
-posiciones_tomadas = []
-
-for i in range(n-1):
-    checando = ar[i]
-    if i in posiciones_tomadas:
-        pass
-    else:
-        print (checando)
-        for o in range(i+1, n):
-            if ar[i] == ar[o]:
-                cant_pares = cant_pares + 1
-                posiciones_tomadas.append(i)
-                posiciones_tomadas.append(o)
-
-print ("posiciones tomadas: ",posiciones_tomadas)
-print ("pares encontrados: ",cant_pares)
+print (n)
+print (ar)
+pares_encontrados = sockMerchant(n,ar)
+print (pares_encontrados)
